1e294e09a1e3bf5ce07089608870b4f15b818ba6
Atul Verma <47239011+VAtul-Verma@users.noreply.github.com>
2022-03-15 12:21:34 +0530

    Time: 14 ms (31.99%), Space: 51.7 MB (45.11%) - LeetHub

--- 914-x-of-a-kind-in-a-deck-of-cards/914-x-of-a-kind-in-a-deck-of-cards.java
+++ 914-x-of-a-kind-in-a-deck-of-cards/914-x-of-a-kind-in-a-deck-of-cards.java
@@ -1,23 +1,19 @@
 class Solution {
-    public boolean hasGroupsSizeX(int[] deck) {
-        HashMap<Integer,Integer>map=new HashMap<>();
-        for(int val:deck){
-            map.put(val,map.getOrDefault(val,0)+1);
-        }
-        int gcd=1;
-        int cnt=0;
-        for(int key:map.keySet()){
-            if(cnt==0){
-                gcd=map.get(key);
-            }else{
-            gcd=Gcd(map.get(key),gcd);
-            }
-            cnt++;
-        }
-        return (gcd>=2?true:false);
+       public boolean hasGroupsSizeX(int[] deck) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < deck.length; i++)
+            map.put(deck[i], map.getOrDefault(deck[i], 0) + 1);
+        int ans = 0;
+        for(int key : map.keySet())
+            ans = gcd(ans, map.get(key));
+        if(ans == 1)
+            return false;
+        return true;
     }
-    public int Gcd(int a, int b) {
-   if (b==0) return a;
-   return Gcd(b,a%b);
-}
+    
+    public int gcd(int a, int b) {
+		if(a == 0)
+			return b;
+		return gcd(b % a, a);
+	}
 }
