94fad9d1eb9f028c2dd285cd5276f1eb4d68e727
Atul Verma <47239011+VAtul-Verma@users.noreply.github.com>
2022-04-06 11:56:37 +0530

    Time: 2 ms (92.89%), Space: 49.8 MB (50.26%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.java
+++ 229-majority-element-ii/229-majority-element-ii.java
@@ -1,15 +1,36 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        HashMap<Integer,Integer>map=new HashMap<>();
-        for(int val:nums){
-            map.put(val,map.getOrDefault(val,0)+1);
-        }
-        List<Integer>ans=new ArrayList<>();
-        for(int key:map.keySet()){
-            if(map.get(key)>nums.length/3){
-                ans.add(key);
+        int num1=Integer.MIN_VALUE;
+        int num2=Integer.MIN_VALUE;;
+        int cnt1=0;
+        int  cnt2=0;
+        for(int i:nums){
+            if(i==num1)cnt1++;
+            else if(i==num2)cnt2++;
+            else if(cnt1==0){
+                num1=i;
+                cnt1=1;
+            }else if(cnt2==0){
+                num2=i;
+                cnt2=1;
+            }else {
+                cnt1--;
+                cnt2--;
             }
         }
+        List<Integer>ans=new ArrayList<>();
+        cnt1=0;
+        cnt2=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==num1){
+                cnt1++;
+            }else if(nums[i]==num2){
+                cnt2++;
+            }
+        }
+        if(cnt1>nums.length/3)ans.add(num1);
+        if(cnt2>nums.length/3)ans.add(num2);
         return ans;
+        
     }
 }
