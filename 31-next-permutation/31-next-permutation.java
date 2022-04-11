f05cd680585b9a1534556d5b1ac4d08e42e4c52c
Atul Verma <47239011+VAtul-Verma@users.noreply.github.com>
2022-04-11 15:03:53 +0530

    Time: 1 ms (85.65%), Space: 43.8 MB (46.00%) - LeetHub

--- 31-next-permutation/31-next-permutation.java
+++ 31-next-permutation/31-next-permutation.java
@@ -1,26 +1,28 @@
 class Solution {
-    public void nextPermutation(int[] arr) {
-        if(arr==null || arr.length==1)return ;
-        int i=arr.length-2;
-        while(i>=0 && arr[i]>=arr[i+1]){
+    public void nextPermutation(int[] nums) {
+        if(nums.length==0 || nums.length==1){
+            return ;
+        }
+        int i=nums.length-2;
+        while(i>=0 && nums[i]>=nums[i+1]){
             i--;
         }
         if(i>=0){
-            int j=arr.length-1;
-            while(arr[j]<=arr[i]){
-                j--;
-            }
-            int temp=arr[j];
-            arr[j]=arr[i];
-            arr[i]=temp;
+        int j=nums.length-1;
+        while(nums[i]>=nums[j]){
+            j--;
         }
-        reverse(arr,i+1,arr.length-1);
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+        }
+        reverse(nums,i+1,nums.length-1);
     }
-    public void reverse(int []arr,int l,int r){
-        while(l<=r){
-            int temp=arr[l];
-            arr[l]=arr[r];
-            arr[r]=temp;
+    public static void reverse(int []nums,int l,int r){
+        while(l<r){
+            int temp=nums[l];
+            nums[l]=nums[r];
+            nums[r]=temp;
             l++;
             r--;
         }
