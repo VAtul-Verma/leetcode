8b49518ca7dd0649355604f13386d963f9ea00f3
Atul Verma <47239011+VAtul-Verma@users.noreply.github.com>
2022-04-11 14:25:08 +0530

    Time: 1 ms (89.09%), Space: 54.1 MB (50.68%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -1,33 +1,28 @@
 class Solution {
-    public void setZeroes(int[][] arr) {
-        int []row=new int[arr.length];
-        Arrays.fill(row,Integer.MIN_VALUE);
-       
-        int []col=new int[arr[0].length];
-         Arrays.fill(col,Integer.MIN_VALUE);
-        for(int i=0;i<arr.length;i++){
-            for(int j=0;j<arr[0].length;j++){
-                if(arr[i][j]==0){
-                    row[i]=0;
-                    col[j]=0;
+    public void setZeroes(int[][] m) {
+    int []row=new int[m.length];
+        int []col=new int[m[0].length];
+        for(int i=0;i<m.length;i++){
+            for(int j=0;j<m[0].length;j++){
+                if(m[i][j]==0){
+                    row[i]=1;
+                    col[j]=1;
                 }
             }
         }
-        for(int val:row){
-            System.out.print(val+" ");
-        }
-        System.out.println(0);
-         for(int val:col){
-            System.out.print(val+" ");
-        }
-         for(int i=arr.length-1 ;i>=0;i--){
-            for(int j=arr[0].length-1;j>=0;j--){
-                if(row[i]==0 || col[j]==0){
-                    arr[i][j]=0;
+        for(int i=0;i<row.length;i++){
+            if(row[i]==1){
+                for(int k=0;k<m[0].length;k++){
+                    m[i][k]=0;
                 }
             }
         }
-        
-        
+         for(int i=0;i<col.length;i++){
+            if(col[i]==1){
+                for(int k=0;k<m.length;k++){
+                    m[k][i]=0;
+                }
+            }
+        }
     }
 }
