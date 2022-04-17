a7b4bc19f0557e4ab302af16cd3a7001ac7ac555
Atul Verma <47239011+VAtul-Verma@users.noreply.github.com>
2022-04-17 14:44:46 +0530

    Time: 2 ms (95.67%), Space: 42.1 MB (97.37%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.java
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.java
@@ -1,28 +1,21 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-     Arrays.sort(nums1);
-        Arrays.sort(nums2);
-        int i=0;
-        int j=0;
-        ArrayList<Integer>ans=new ArrayList<>();
-        
-        while(i!=nums1.length && j!=nums2.length){
-            if(nums1[i]==nums2[j]){
-            ans.add(nums1[i]);
-                i++;
-                j++;
-            }else if(nums1[i]<nums2[j]){
-                i++;
-            }else {
-                j++;
-            }
-        }int k=0;
-        int []res=new int[ans.size()];
-        for(int val:ans){
-            res[k]=val;
-            k++;
+       ArrayList<Integer>insec=new ArrayList<>();
+        HashMap<Integer,Integer>map=new HashMap<>();
+        for(int i:nums1){
+            map.put(i,map.getOrDefault(i,0)+1);
         }
-        return res;
-        
+        for(int val:nums2){
+            if(map.containsKey(val) && map.get(val)>0){
+                map.put(val,map.get(val)-1);
+                insec.add(val);
+            }
+        }
+        int []ans=new int[insec.size()];
+        int k=0;
+        for(int i:insec){
+            ans[k++]=i;
+        }
+        return ans;
     }
 }
