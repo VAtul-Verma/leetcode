5a97ab8c32fe7865f5c998dce93ef38d291ad08e
Atul Verma <47239011+VAtul-Verma@users.noreply.github.com>
2022-04-25 11:59:58 +0530

    Time: 0 ms (100.00%), Space: 43.4 MB (33.68%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.java
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.java
@@ -19,7 +19,8 @@ class Solution {
             return 0;
         }
         int left=maxDepth(root.left);
-        int right=maxDepth(root.right);
+        int  right= maxDepth(root.right);
         return Math.max(left,right)+1;
     }
+    
 }
