f8faf8de4464ef9bd093313f7547b774c8a1c72d
Atul Verma <47239011+VAtul-Verma@users.noreply.github.com>
2022-05-01 12:11:30 +0530

    Time: 1 ms (87.56%), Space: 42.4 MB (38.73%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.java
+++ 844-backspace-string-compare/844-backspace-string-compare.java
@@ -1,26 +1,47 @@
 class Solution {
-    public boolean backspaceCompare(String s, String t) {
-       String st=convert(s);
-        String st1=convert(t);
-        return st.equals(st1);
-       
-    }
-    public static String convert(String s){
-          Stack<Character>st=new Stack<>();
-        for(int i=0;i<s.length();i++){
-            char ch=s.charAt(i);
-            if(ch=='#'){
-                if(st.size()>0){
-                    st.pop();
-                }
-            }else{
-                st.push(ch);
+   public boolean backspaceCompare(String S, String T) {
+	assert S != null && T != null;
+
+    // Iterate to find next pair each time
+    int i = S.length() - 1;
+    int j = T.length() - 1;
+    while (i >= 0 || j >= 0) {
+        // Skip deleted elements in both strings, until next effective character
+        i = skipBackspaceCharacters(S, i);
+        j = skipBackspaceCharacters(T, j);
+
+        // Compare character pair
+        if (i >= 0 && j >= 0) {
+            if (S.charAt(i) != T.charAt(j)) {
+                return false;
             }
+
+            i--;
+            j--;
+        } else if ((i >= 0) != (j >= 0)) {    // Unmatched indices
+            return false;
         }
-        StringBuilder str=new StringBuilder();
-        while(!st.empty()){
-            str.append(st.pop());
-        }
-        return str.toString();
     }
+
+    return true;
+}
+
+private int skipBackspaceCharacters(String s, int index) {
+    // Simulate stack in & out sequence with counter
+    while (index >= 0 && s.charAt(index) == '#') {
+        int count = 1;
+        index--;
+
+        while (index >= 0 && count > 0) {
+            if (s.charAt(index) == '#') {
+                count++;
+            } else {
+                count--;
+            }
+            index--;
+        }
+    }
+
+    return index;
+}
 }
