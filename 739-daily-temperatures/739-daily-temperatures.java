0573eccf655691f3fe40541bce95f7bb1708c6fe
Atul Verma <47239011+VAtul-Verma@users.noreply.github.com>
2022-05-01 12:40:13 +0530

    Time: 2178 ms (5.00%), Space: 108.9 MB (71.40%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.java
+++ 739-daily-temperatures/739-daily-temperatures.java
@@ -5,21 +5,17 @@ class Solution {
             boolean flag=true;
             for(int j=i+1;j<nums.length;j++){
                 if(nums[i]<nums[j]){
-                    ans.add(j-i);
+                   nums[i]=(j-i);
                     flag=false;
                     break;
                 }
             }
             if(flag==true){
-                ans.add(0);
+               nums[i]=0;
             }
         }
-        ans.add(0);
-        int []fans=new int[ans.size()];
-        int j=0;
-        for(int i:ans){
-            fans[j++]=i;
-        }
-        return fans;
+      nums[nums.length-1]=0;
+       
+        return nums;
     }
 }
