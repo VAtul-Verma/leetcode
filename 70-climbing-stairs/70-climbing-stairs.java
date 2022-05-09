126fb1d8d49b7a512a7c6731c36484c2e7e5aff6
Atul Verma <47239011+VAtul-Verma@users.noreply.github.com>
2022-05-09 23:24:04 +0530

    Time: 0 ms (100.00%), Space: 39.2 MB (77.95%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -6,12 +6,33 @@ class Solution {
         
     }
     public int climbStairshelper(int n,int []dp) {
-       if(n<0)return dp[0]=0;
-        if(n==0)return dp[n]= 1;
-        if(dp[n]!=0)return dp[n];
+      for(int i=0;i<=n;i++){
+          if(i<0){
+              dp[i]=0;
+          }
+        if(i==0 || i==1){
+            dp[i]= 1;
+            continue;
+        }
+      
         
-        return dp[n]=climbStairshelper(n-1,dp)+climbStairshelper(n-2,dp);
+    dp[i]=dp[i-1]+dp[i-2];
         
+      }
+        return dp[n];
     }
+    
+    
+    
+    
+    //memo
+//      public int climbStairshelper(int n,int []dp) {
+//        if(n<0)return dp[0]=0;
+//         if(n==0)return dp[n]= 1;
+//         if(dp[n]!=0)return dp[n];
+        
+//         return dp[n]=climbStairshelper(n-1,dp)+climbStairshelper(n-2,dp);
+        
+//     }
     
 }
