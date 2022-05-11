dbcd909703ce9260cec4e5014d356dc4f8489fec
Atul Verma <47239011+VAtul-Verma@users.noreply.github.com>
2022-05-11 22:50:19 +0530

    Time: 2362 ms (5.01%), Space: 46.5 MB (43.56%) - LeetHub

---
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int longestCommonSubsequence(String s1, String s2) {
+        int n=s1.length();
+        int m=s2.length();
+        int [][]dp=new int[n+1][m+1];
+       return longestCommonSubsequence_memo(s1,s2,n,m,dp);
+        
+        
+    }
+    public static int longestCommonSubsequence_memo(String s1,String s2,int n,int m,int [][]dp){
+        if(n==0 || m==0){
+            return dp[n][m]=0;    //no common subsequence
+        }
+        if(dp[n][m]!=0){
+            return dp[n][m];
+        }
+        int a=longestCommonSubsequence_memo(s1,s2,n-1,m-1,dp);
+        int b=longestCommonSubsequence_memo(s1,s2,n-1,m,dp);
+        int c=longestCommonSubsequence_memo(s1,s2,n,m-1,dp);
+        if(s1.charAt(n-1)==s2.charAt(m-1)){
+            return dp[n][m]=a+1;
+        }else{
+            return dp[n][m]=Math.max(b,c);
+        }
+    }
+}
