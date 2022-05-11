b81ef44056bf9c3e29c188970d8effb1146a0e38
Atul Verma <47239011+VAtul-Verma@users.noreply.github.com>
2022-05-11 10:27:21 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (35.01%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.java
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.java
@@ -1,28 +1,28 @@
-class Solution {
-    char[] ch=new char[]{'a','e','i','o','u'};
-    public int countVowelStrings(int n) {     //recursive code
-        int ans=0;
-        for(char c:ch){
-            ans+=count(n-1,c);
-        }
+// class Solution {
+//     char[] ch=new char[]{'a','e','i','o','u'};
+//     public int countVowelStrings(int n) {     //recursive code
+//         int ans=0;
+//         for(char c:ch){
+//             ans+=count(n-1,c);
+//         }
         
-        return ans;
-    }
+//         return ans;
+//     }
     
-    int count(int length,char first){
-        if(length==0){
-            return 1;
-        }
-        int ans=0;
-        for(char c:ch){
-            if(first>=c){
-                ans+=count(length-1,c);
-            }
-        }
+//     int count(int length,char first){
+//         if(length==0){
+//             return 1;
+//         }
+//         int ans=0;
+//         for(char c:ch){
+//             if(first>=c){
+//                 ans+=count(length-1,c);
+//             }
+//         }
         
-        return ans;
-    }
-}
+//         return ans;
+//     }
+// }
 
 // class Solution {
 //     public int countVowelStrings(int n) {   //using dp   TC=(5N)  SC=(6N)
@@ -38,8 +38,8 @@ class Solution {
 //     }
 // }
 
-// class Solution {
-//     public int countVowelStrings(int n) {      //maths formula   (n+k-1)!/k!.(n-1)!
-//         return (n+4)*(n+3)*(n+2)*(n+1)/24;
-//     }
-// }
+class Solution {
+    public int countVowelStrings(int n) {      //maths formula   (n+k-1)!/k!.(n-1)!
+        return (n+4)*(n+3)*(n+2)*(n+1)/24;
+    }
+}
