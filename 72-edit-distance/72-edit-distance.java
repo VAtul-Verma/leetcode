e4f10af687f962acdc942852b95fda9790a9ed20
Atul Verma <47239011+VAtul-Verma@users.noreply.github.com>
2022-05-12 20:32:02 +0530

    Time: 12 ms (16.61%), Space: 44.9 MB (47.28%) - LeetHub

--- 72-edit-distance/72-edit-distance.java
+++ 72-edit-distance/72-edit-distance.java
@@ -5,20 +5,27 @@ class Solution {
         int dp[][]=new int[n+1][m+1];
          return helper(word1,word2,n,m,dp);
     }
-    public int helper(String s1,String s2,int n,int m,int [][]dp){
-        if(n==0 || m==0){
-            return dp[n][m]=n!=0?n:m;
+    public int helper(String s1,String s2,int N,int M,int [][]dp){
+        for(int n=0;n<=N;n++){
+            for(int m=0;m<=M;m++){
+                 if(n==0 || m==0){
+             dp[n][m]=n!=0?n:m;
+                     continue;
         }
-        if(dp[n][m]!=0)return dp[n][m];
+       
         
-        int a=helper(s1,s2,n-1,m-1,dp);   //replace call;
-         int b=helper(s1,s2,n,m-1,dp); //insert call;
-         int c=helper(s1,s2,n-1,m,dp); //delete call;
+        int a=dp[n-1][m-1];   //replace call;
+         int b=dp[n-1][m] ;//insert call;
+         int c=dp[n][m-1]; //delete call;
         
         if(s1.charAt(n-1)==s2.charAt(m-1)){
-            return dp[n][m]=a;
+           dp[n][m]=a;
         }else{
-            return dp[n][m]=Math.min(Math.min(a,b),c)+1;
+            dp[n][m]=Math.min(Math.min(a,b),c)+1;
         }
+            }
+        }
+        return dp[N][M];
+       
     }
 }
