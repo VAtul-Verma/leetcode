23bd4a42405162d73070958c1b8edafc7e82e310
Atul Verma <47239011+VAtul-Verma@users.noreply.github.com>
2022-05-13 11:09:08 +0530

    Time: 32 ms (28.30%), Space: 50.7 MB (17.60%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.java
+++ 115-distinct-subsequences/115-distinct-subsequences.java
@@ -8,17 +8,29 @@ class Solution {
         }
         return helper(s,t,n,m,dp);
     }
-    public int helper(String s1,String s2,int n,int m,int [][]dp){
-       if(m==0)return dp[n][m]=1;
-        else if(n==0)return dp[n][m]=0;   //no way to found answer
-        if(dp[n][m]!=-1)return dp[n][m];
-        int a=helper(s1,s2,n-1,m-1,dp)  ; //when character are same
-        int b=helper(s1,s2,n-1,m,dp);
+    public int helper(String s1,String s2,int N,int M,int [][]dp){
+        for(int n=0;n<=N;n++){
+            for(int m=0;m<=M;m++){
+        if(m==0) {
+            dp[n][m]=1;
+            continue;
+        }
+        else if(n==0){
+             dp[n][m]=0;
+            continue;
+        }   
+       
+        int a=dp[n-1][m-1]  ; //when character are same
+        int b=dp[n-1][m];
       
         if(s1.charAt(n-1)==s2.charAt(m-1)){
-            return dp[n][m]=a+b;
+            dp[n][m]=a+b;
         }else{
-            return dp[n][m]=b;
+         dp[n][m]=b;
         }
+            }
+        }
+        return dp[N][M];
+      
     }
 }
