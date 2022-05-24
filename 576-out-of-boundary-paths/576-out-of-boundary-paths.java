e49bd1fdbe6cfed93ad1a5f5c0e7250aa4643cf5
Atul Verma <47239011+VAtul-Verma@users.noreply.github.com>
2022-05-24 22:38:22 +0530

    Time: 7 ms (70.14%), Space: 42.6 MB (55.62%) - LeetHub

---
+++ 576-out-of-boundary-paths/576-out-of-boundary-paths.java
@@ -0,0 +1,27 @@
+class Solution {
+    int [][]dir={ {1,0},{0,1},{0,-1},{-1,0}    };
+    int mod=1000000007;
+    public int findPaths(int n, int m, int k, int startRow, int startColumn) {
+        int [][][]dpp=new int[n+1][m+1][k+1];
+        for(int [][]dp:dpp){
+            for(int []d:dp){
+                Arrays.fill(d,-1);
+            }
+        }
+        return findPathshelper(n,m,k,startRow,startColumn,dpp);
+    }
+     public int findPathshelper(int n, int m, int k, int sr, int sc,int [][][]dp) {
+        if(sr<0 || sc<0 || sr==n || sc==m){
+            return k>=0?1:0;
+        }
+         if(k==0)return 0;
+         if(dp[sr][sc][k]!=-1)return dp[sr][sc][k];
+         int cnt=0;
+         for(int []d:dir){
+             int r=sr+d[0];
+             int c=sc+d[1];
+             cnt=(cnt+findPathshelper(n,m,k-1,r,c,dp))%mod;
+         }
+         return dp[sr][sc][k]=cnt;
+    }
+}
