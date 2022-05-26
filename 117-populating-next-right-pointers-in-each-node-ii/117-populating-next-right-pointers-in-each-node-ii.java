d318d1bdf052cfb27c6f154236ec725c41613b30
Atul Verma <47239011+VAtul-Verma@users.noreply.github.com>
2022-05-26 21:57:22 +0530

    Time: 1 ms (73.87%), Space: 46.3 MB (5.38%) - LeetHub

--- 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.java
+++ 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.java
@@ -25,43 +25,43 @@ class Solution {
     
     ///  BFS TRAVERSAL    
     // TC=O(N)  SC=O(N)
-    // public Node connect(Node root) {
-    //     if(root==null)return null;
-    //     Queue<Node>q=new LinkedList<>();
-    //     q.add(root);
-    //     while(q.size()!=0){
-    //         int size=q.size();
-    //         Node dummy=new Node(0);
-    //         while(size-->0){
-    //             Node temp=q.remove();
-    //             dummy.next=temp;
-    //             dummy=dummy.next;
-    //             if(temp.left!=null)q.add(temp.left);
-    //             if(temp.right!=null)q.add(temp.right);
-    //         }
-    //     }
-    //     return root;
-    // }
-     public Node connect(Node root) {
+    public Node connect(Node root) {
         if(root==null)return null;
-       Node head=root;
-        while(head!=null){
-            
+        Queue<Node>q=new LinkedList<>();
+        q.add(root);
+        while(q.size()!=0){
+            int size=q.size();
             Node dummy=new Node(0);
-            Node temp=dummy;
-            while(head!=null){
-               if(head.left!=null){
-                   temp.next=head.left;
-                   temp=temp.next;
-               }
-                 if(head.right!=null){
-                   temp.next=head.right;
-                   temp=temp.next;
-               }
-                head=head.next;
+            while(size-->0){
+                Node temp=q.remove();
+                dummy.next=temp;
+                dummy=dummy.next;
+                if(temp.left!=null)q.add(temp.left);
+                if(temp.right!=null)q.add(temp.right);
             }
-            head=dummy.next;
         }
         return root;
     }
+//      public Node connect(Node root) {
+//         if(root==null)return null;
+//        Node head=root;
+//         while(head!=null){
+            
+//             Node dummy=new Node(0);
+//             Node temp=dummy;
+//             while(head!=null){
+//                if(head.left!=null){
+//                    temp.next=head.left;
+//                    temp=temp.next;
+//                }
+//                  if(head.right!=null){
+//                    temp.next=head.right;
+//                    temp=temp.next;
+//                }
+//                 head=head.next;
+//             }
+//             head=dummy.next;
+//         }
+//         return root;
+//     }
 }
