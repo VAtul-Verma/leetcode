804792db2293b93848cf5ba2174b844c24e92a69
Atul Verma <47239011+VAtul-Verma@users.noreply.github.com>
2022-05-26 22:24:23 +0530

    Added solution - LeetHub

---
+++ Permutations of a given string - GFG/permutations-of-a-given-string.java
@@ -0,0 +1,58 @@
+// { Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+	public static void main(String[] args) throws IOException
+	{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while(t-->0)
+        {
+            String S = br.readLine().trim();
+            Solution obj = new Solution();
+            List<String> ans = obj.find_permutation(S);
+            for( int i = 0; i < ans.size(); i++)
+            {
+                System.out.print(ans.get(i)+" ");
+            }
+            System.out.println();
+                        
+        }
+	}
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public List<String> find_permutation(String S) {
+        // Code here
+        List<String>fans=new ArrayList<>();
+        HashSet<String>ans=new HashSet<>();
+         helper(S,"",ans);
+         for(String key:ans){
+            
+                 fans.add(key);
+                
+            
+         }
+       Collections.sort(fans);
+         return fans;
+    }
+    public void helper(String s,String psf,HashSet<String>ans){
+        if(s.length()==0){
+           
+               ans.add(psf);
+           
+            return;
+        }
+    for(int i=0;i<s.length();i++){
+        helper(s.substring(0,i)+s.substring(i+1),psf+s.charAt(i),ans);
+    }
+        
+        
+        
+    }
+}
