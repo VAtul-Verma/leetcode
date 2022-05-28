138649ab3cacde7b04983590ad8bea4a88fe37a0
Atul Verma <47239011+VAtul-Verma@users.noreply.github.com>
2022-05-28 17:31:51 +0530

    Time: 1 ms (82.22%), Space: 43.8 MB (36.36%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
@@ -1,28 +1,15 @@
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
-        int []dp=new int[cost.length+1];
-        return minCostClimbingStairshelper(cost.length,cost,dp);
+     int n=cost.length;
+        int dp[]=new int[n+1];
+        return minCostClimbingStairs(cost.length,cost,dp) ;
     }
-    public int minCostClimbingStairshelper(int N,int []cost,int []dp){
-        for(int i=0;i<=N;i++){
-            if(i<=1){dp[i]=cost[i];
-                     continue;}
-        
-        int fcall=dp[i-1];
-        int scall=dp[i-2];
-        int ans=Math.min(fcall,scall)+(i==cost.length?0:cost[i]);
-         dp[i]= ans;
-        }
-        return dp[N];
+     public int minCostClimbingStairs(int n,int[] cost,int []dp) {
+        if(n<=1)return dp[n]=cost[n];
+         if(dp[n]!=0)return dp[n];
+         int fcall=minCostClimbingStairs(n-1,cost,dp);
+         int scall=minCostClimbingStairs(n-2,cost,dp);
+         int ans=Math.min(fcall,scall)+(n==cost.length?0:cost[n]);
+        return dp[n]=ans;
     }
-    
-    //dp memo
-    //  public int minCostClimbingStairshelper(int n,int []cost,int []dp){
-    //     if(n<=1)return dp[n]=cost[n];
-    //     if(dp[n]!=0)return dp[n];
-    //     int fcall=minCostClimbingStairshelper(n-1,cost,dp);
-    //     int scall=minCostClimbingStairshelper(n-2,cost,dp);
-    //     int ans=Math.min(fcall,scall)+(n==cost.length?0:cost[n]);
-    //     return dp[n]= ans;
-    // }
 }
