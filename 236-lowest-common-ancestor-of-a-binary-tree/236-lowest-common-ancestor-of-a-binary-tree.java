0075477c78f3b8ab7a30117c7b9040591006cf9f
Atul Verma <47239011+VAtul-Verma@users.noreply.github.com>
2022-06-01 00:12:34 +0530

    Time: 9 ms (52.27%), Space: 47 MB (61.08%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
@@ -9,34 +9,49 @@
  */
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        ArrayList<TreeNode>ans=new ArrayList<>();
-        ArrayList<TreeNode>ans2=new ArrayList<>();
-        nodetorootpath(root,p,ans);
-         nodetorootpath(root,q,ans2);
-        int i=ans.size()-1;
-        int j=ans2.size()-1;
-        while(i>=0 && j>=0){
-            if(ans.get(i)!=ans2.get(j))break;
-            i--;
-            j--;
-        }
-        return ans.get(i+1);
-        
-        
+        Lca(root,p,q);
+        return LCA;
     }
-    public static boolean nodetorootpath(TreeNode root,TreeNode p,ArrayList<TreeNode>ans){
-        if(root==null)return false ;
-        if(root==p){
-            ans.add(root);
-            return true;
+//     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+//         ArrayList<TreeNode>ans=new ArrayList<>();
+//         ArrayList<TreeNode>ans2=new ArrayList<>();
+//         nodetorootpath(root,p,ans);
+//          nodetorootpath(root,q,ans2);
+//         int i=ans.size()-1;
+//         int j=ans2.size()-1;
+//         while(i>=0 && j>=0){
+//             if(ans.get(i)!=ans2.get(j))break;
+//             i--;
+//             j--;
+//         }
+//         return ans.get(i+1);
+        
+        
+//     }
+    TreeNode LCA=null;
+    public boolean Lca(TreeNode root,TreeNode p,TreeNode q){
+        if(root==null)return false;
+        boolean self=(root==p || root==q);
+        boolean left=Lca(root.left,p,q);
+        boolean right=Lca(root.right,p,q);
+        if((left && right)|| (left && self)||(right && self)){
+            LCA=root;
+        }
+        return left || right|| self;
+    }
+//     public static boolean nodetorootpath(TreeNode root,TreeNode p,ArrayList<TreeNode>ans){
+//         if(root==null)return false ;
+//         if(root==p){
+//             ans.add(root);
+//             return true;
             
-        }
-      boolean left=  nodetorootpath(root.left,p,ans);
-        boolean right=nodetorootpath(root.right,p,ans);
-        if(left || right){
-            ans.add(root);
-        }
-        return left || right;
+//         }
+//       boolean left=  nodetorootpath(root.left,p,ans);
+//         boolean right=nodetorootpath(root.right,p,ans);
+//         if(left || right){
+//             ans.add(root);
+//         }
+//         return left || right;
         
-    }
+//     }
 }
