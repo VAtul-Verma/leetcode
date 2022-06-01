7f8937e0f8012c749864972c19ffedcaaf09692e
Atul Verma <47239011+VAtul-Verma@users.noreply.github.com>
2022-06-01 21:16:10 +0530

    Time: 7 ms (45.74%), Space: 49.7 MB (59.81%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.java
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -9,18 +9,34 @@
  */
 
 class Solution {
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root==null || root==p || root==q){
-            return root;
-        }
-        TreeNode left=lowestCommonAncestor(root.left,p,q);
-        TreeNode right=lowestCommonAncestor(root.right,p,q);
-        if(left==null){   //always retur not null node
-            return right;
-        }else if(right==null)return left;
-        else{//if both are not null value the we found lca;
-            return root;
+//     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+//         if(root==null || root==p || root==q){
+//             return root;
+//         }
+//         TreeNode left=lowestCommonAncestor(root.left,p,q);
+//         TreeNode right=lowestCommonAncestor(root.right,p,q);
+//         if(left==null){   //always retur not null node
+//             return right;
+//         }else if(right==null)return left;
+//         else{//if both are not null value the we found lca;
+//             return root;
             
+//         }
+//     }
+        public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+            if(root==null)return null;
+            TreeNode curr=root;
+            TreeNode lca=null;
+            while(curr!=null){
+                if(curr.val<p.val && curr.val<q.val){
+                    curr=curr.right;
+                }else  if(curr.val>p.val && curr.val>q.val){
+                    curr=curr.left;
+                }else{
+                    lca=curr;
+                    break;
+                }
+            }
+            return lca;
         }
-    }
 }
