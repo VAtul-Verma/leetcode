a0918029df9dc5ce9e65fbafd83d8de799e6e84f
Atul Verma <47239011+VAtul-Verma@users.noreply.github.com>
2022-08-28 20:32:21 +0530

    Time: 8 ms (74.83%), Space: 48 MB (56.25%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.java
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.java
@@ -1,16 +1,33 @@
 class Solution {
     public int[][] diagonalSort(int[][] mat) {
-        Map<Integer,PriorityQueue<Integer>>map=new HashMap();
-        for(int i=0;i<mat.length;i++){
-            for(int j=0;j<mat[0].length;j++){
-                map.computeIfAbsent(i-j,p->new PriorityQueue()).add(mat[i][j]);
-            }
+        int m=mat.length;
+        int n=mat[0].length;
+        for(int col=0;col<n;col++){
+            sort(mat,0,col,m,n);
         }
-        for(int i=0;i<mat.length;i++){
-            for(int j=0;j<mat[0].length;j++){
-                mat[i][j]=map.get(i-j).poll();
-            }
-            }
+        for(int row=1;row<m;row++){
+            sort(mat,row,0,m,n);
+        }
         return mat;
+        
     }
-}
+    public void sort(int [][]mat,int row,int col,int m,int n){
+        int r=row;
+        int c=col;
+        List<Integer>ans=new ArrayList<Integer>();
+        while(r<m && c<n){
+            ans.add(mat[r][c]);
+            r++;
+            c++;
+    }
+    Collections.sort(ans);
+    r=row;
+        c=col;
+        int index =0;
+          while(r<m && c<n){
+            mat[r][c]=ans.get(index++);
+            r++;
+            c++;
+    }
+        
+}}
